e15dcf92aedf33fa67fdbafa1d0c8f0040aa703a
473856 <473856@posteo.org>
2015-03-21 07:34:51 +0100

    150321 mqtt_to_emoncms 1.04 beta - subscribe in connect

--- mqtt/mqtt_to_emoncms.py
+++ mqtt/mqtt_to_emoncms.py
@@ -9,7 +9,7 @@
 # - sending to YunHub 1.03 MQTT
 #
 
-STARTUP_MESSAGE_VERSION = '150318 mqtt_to_emoncms 1.03'
+STARTUP_MESSAGE_VERSION = '150321 mqtt_to_emoncms 1.04 beta'
 
 import StringIO
 import csv
@@ -21,8 +21,12 @@ from mytokens import EMONCMS_WRITE_API_KEY
 emoncms_url_init = 'http://emoncms.org/input/post.json?apikey=' + EMONCMS_WRITE_API_KEY
 
 
-def on_connect(mqttc, obj, flags, rc):
-    print('mqtt connect established, rc: ' + str(rc))
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(mqttc, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    mqttc.subscribe("TNode", 0)
 
 
 def on_disconnect(mqttc, obj, flags, rc):
@@ -42,17 +46,15 @@ def on_message(mqttc, obj, msg):
         'T_in'] + ',' + TNodeData['T_out']
     print emoncms_url
 
-    # send data to emoncms, incl. error handling.
-    # Use requests instead of urllib2 to catch [Errno 54] Connection reset by peer
+    # send data to emoncms & handle exceptions.
+    # Use requests instead of urllib2 to catch exceptions.
     try:
         response = requests.get(emoncms_url, timeout=1)
-    except requests.ConnectionError as e:  # This is the correct syntax
+    except requests.ConnectionError as e:
         print e
-        response = 'No response'
-        pass
+        response = 'ConnectionError execption: No response'
     except:
         response = 'Unidentified exception ...'
-        pass
 
     print response
     print
@@ -73,6 +75,7 @@ def on_unsubscribe(mqttc, obj, mid, granted_qos):
 def on_log(mqttc, obj, level, string):
     print(string)
 
+
 # If you want to use a specific client id, use mqttc = mqtt.Client("client-id")
 # but note that the client id must be unique on the broker. Leaving the client
 # id parameter empty will generate a random id for you.
@@ -84,7 +87,7 @@ mqttc.on_subscribe = on_subscribe
 mqttc.on_unsubscribe = on_unsubscribe
 
 # Uncomment to enable debug messages
-#mqttc.on_log = on_log
+# mqttc.on_log = on_log
 
 print '###'
 print '###'
@@ -92,11 +95,11 @@ print '### ' + STARTUP_MESSAGE_VERSION
 print '###'
 print '###'
 print
+
+# connect to mqtt server. Subscription takes place in on_connect
 mqttc.connect("192.168.1.12", 1883, 60)
-mqttc.subscribe("TNode", 0)
 
-rc = 0
-while rc == 0:
+while True:
     rc = mqttc.loop()
-
-print("rc: " + str(rc))
+    if rc <> 0:
+        print("mqttc loop error, rc: " + str(rc))
